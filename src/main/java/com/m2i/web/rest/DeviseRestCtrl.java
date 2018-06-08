4cf7c473645394a0c6e2bef8aecf5c0c5c8103c3
didier <didier@ici_ou_labas.fr>
2018-06-08 16:36:35 +0200

    nouvelle version

--- src/main/java/com/m2i/web/rest/DeviseRestCtrl.java
+++ src/main/java/com/m2i/web/rest/DeviseRestCtrl.java
@@ -4,6 +4,9 @@ package com.m2i.web.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,23 @@ public class DeviseRestCtrl {
 		//NB: le resultat java de type List<Devise>
 		//sera automatiquement transformé au format JSON
 	}
+	
+	//sera appelé en HTTP / GET avec l' URL suivante:
+	// http://localhost:8080/tpSpringWeb/mvc/rest/devise/EUR
+	@RequestMapping(value="/{code}" , method=RequestMethod.GET)
+	ResponseEntity<Devise> getDeviseByCode(@PathVariable(name="code")String code){
+			try {
+				Devise dev = daoDevise.findDeviseByCode(code);
+				if(dev==null)
+					return new ResponseEntity<Devise>(HttpStatus.NOT_FOUND);
+				else
+				    return new ResponseEntity<Devise>(dev,HttpStatus.OK);
+			} catch (Exception e) {
+				//e.printStackTrace();
+				System.err.println(e.getMessage());//ou logger....
+				return new ResponseEntity<Devise>(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+		}
 }
 
